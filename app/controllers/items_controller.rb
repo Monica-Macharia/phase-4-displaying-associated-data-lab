fc7449e72b905e517071017e21b12504024c2237
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-10-01 11:25:50 +0300

    associations

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,11 @@
 class ItemsController < ApplicationController
+    
+    
+    def index
+        items = Item.all.order(price: :desc)
+        render json: items, include: :user
+      end
+
+
+
 end
